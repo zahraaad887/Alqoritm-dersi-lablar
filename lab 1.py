88f96337d83a86910c7cabae0c8c910ef559299f
zahraaad227 <zehraadgozelova@gmail.com>
2025-04-09 11:46:24 +0400

    Create lab 1.py

---
+++ lab 1.py
@@ -0,0 +1,8 @@
+import math as mt
+m=(mt.pow(123,4*123,4*mt.log(2)))/1,124*mt.pow(0.024,1/5)*mt.cos(1)
+
+if k>(-2)*m:
+    netice=(7*k-5*p)/(2*k**2+3*p**2)
+else:
+    netice=abs(k-p)
+print(netice)
